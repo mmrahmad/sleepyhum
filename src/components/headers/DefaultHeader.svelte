d9fb44b81488bd827471d3a6367fe2ec84ed81b1
Ahmad <info@mmrahmad.com>
2022-11-29 23:17:55 +0600

    feat(components): add header components

---
+++ src/components/headers/DefaultHeader.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Container from '$containers/common/Container.svelte';
+	import Logo from '$assets/images/400dpiLogo-scaled.jpg';
+	import MenuItem from './menu/MenuItem.svelte';
+
+	const menuData = [
+		{ id: 1, label: 'Women', path: '#' },
+		{ id: 2, label: 'Mmen', path: '#' },
+		{ id: 3, label: 'Kids', path: '#' },
+		{ id: 4, label: 'Accessories', path: '#' }
+	];
+</script>
+
+<header>
+	<Container class="flex justify-between items-center">
+		<nav class="menu flex-1">
+			<ul class="flex">
+				{#each menuData as menu, i}
+					<MenuItem label={menu.label} path={menu.path} />
+				{/each}
+			</ul>
+		</nav>
+		<div class="flex-1">
+			<img role="img" src={Logo} alt="Logo" class="w-60" />
+		</div>
+		<div class="flex-1">
+			<ul class="flex">
+				<li>test</li>
+				<li>test</li>
+				<li>test</li>
+			</ul>
+		</div>
+	</Container>
+</header>
---
+++ src/components/headers/menu/MenuItem.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let label: string;
+	export let path: string;
+</script>
+
+<li
+	class="py-2 px-2 mx-3 text-gray-800 hover:text-primary-dark font-[500] transition duration-150 border-b-2 border-transparent hover:border-primary"
+>
+	<a href={path}>{label}</a>
+</li>
